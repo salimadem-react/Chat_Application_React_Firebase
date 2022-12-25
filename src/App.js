71ae2cf5a31dded9142a674c3e9d05cdb20f23ee
Salim Salim <salimademm@gmail.com>
2022-12-25 18:02:02 +0200

    create login and register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style.scss";
+import Add from "../img/addAvatar.png";
+
+export default function Login() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+}
